9e25de36c356cb92be8b1bee1c35652540f48d05
ajayappinop <ajay@appinop.com>
2026-09-21 13:33:39 +0530

    new update

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 import News from '../assets/news-1.png';
- import NewsBanner from '../assets/news-banner.jpg';
- 
+import NewsBanner from '../assets/news-banner.jpg';
+import TanyaMikeManoj from '../assets/tanya-mike-and-manoj-at-historic-man-sagar-lake-jaipur-rupali-vardhan.jpg';
+import TanyaMikeHarsh from '../assets/r-to-l-tanya-mike-and-harsh-at-jal-mahal-jaipur-rupali.jpg';
+
 
 function Blog() {
   return (
@@ -32,6 +34,34 @@ function Blog() {
 </div>
       </div>
       </div>
+
+        <div className='card p-5 rounded-xl mt-5'>
+          <div className='row g-5'>
+            <div className='col-xl-7 col-md-12'>
+              <div className='news'>
+                <h3 className='h5 mb-3'>Northern India culture, heritage and wildlife tour with <a href="https://ourindiantours.com/" target="_blank" rel="noreferrer">https://ourindiantours.com/</a></h3>
+                <p>After a friend recommended Manoj Vardhan of Indian based tour company <a href="https://ourindiantours.com/" target="_blank" rel="noreferrer">https://ourindiantours.com/</a> to organise a bespoke 3 week tour for Tanya and myself we took the plunge and did it. Wow - he did an incredible job from hotels, trains to planes, local culture and wildlife guides, drivers and even swimming pools for Tanya - all worked like clockwork. Everything we wanted to see and more! Too much to go into but highlights included the cities, forts, temples and more; Agra and the Taj Mahal, Orchha, Udaipur, Jodhpur, Jhansi and Jaipur. Very friendly and careful drivers and knowledgeable guides. Ayub, who works closely with @ourindiantours was our driver and companion. In Jaipur we were treated to a very special visit to the ‘island’ Jal Mahal Palace and boat trip around the lake with Manoj’s father Harsh, an extraordinary conservationist, plus organiser of the annual Indian Bird Fair (<a href="https://mavericknews30.com/?p=101402" target="_blank" rel="noreferrer">https://mavericknews30.com/?p=101402</a>). Harsh Vardhan played a huge role in restoring the lake in Jaipur from a dried up dump to a beautiful wildlife haven. In the Thar Desert together with the local Bishnoi people, Harsh also fought hard to save the Great Indian Bustard from extinction <a href="https://rajasthanbiodiversity.org/an-extraordinary.../" target="_blank" rel="noreferrer">https://rajasthanbiodiversity.org/an-extraordinary.../</a>, not to mention his work protecting Tigers! We had the privilege of staying with the Vardhan family on the last few days of the tour.</p>
+                <p>And then the wonderful nature reserves of Kaziranga in Assam, Ranthambore in central north, and the Thar Desert in Rajasthan close to the Pakistan border. Completed my schoolboy ambition of one day seeing wild Tigers (including a rare colour morph ‘Golden’ Tiger in Kaziranga), Leopards, finding our own Sloth Bear (Baloo), Indian Rhinos, Asian Elephants, Striped Hyena, Jungle Cat, Otters, deer, antelopes, Water Buffalo, Wild Boar, Mugger Crocs, monkeys, Gibbon, etc. Of course, the birds too highlights: With our Bishnoi ranger guide Great Indian Bustards, seemingly snobbishly aloof, strutting through scrub and desert plains. In Assam, Great Hornbill - whose huge wings rhythmically ‘swashed’ through the air above us. Kheechan at dawn - standing on the roof top of a friendly villager Joti (who was still covered in colourful paint from the night before Holi celebrations) witnessing thousands of trumpeting, Demoiselle Cranes descending from all directions to feed on seed put out for them in the village square. Perhaps only a birder could revel in seeing hundreds of vultures and eagles at a smelly carcass dump that serves as a nature reserve! There were so many other bird highlights it would be difficult to choose even a few more.</p>
+                <p>Luckily our flight wasn’t cancelled due to Trumps war on Iran. However, the direct Air India flight from New Delhi to Heathrow did make an additional, but safe, 3 hour diversion on the way home.</p>
+                <p>Thanks again to <a href="https://ourindiantours.com/" target="_blank" rel="noreferrer">https://ourindiantours.com/</a> and especially Manoj, the Vardhan family and Ayub.</p>
+              </div>
+            </div>
+
+            <div className='col-xl-5 col-md-12'>
+              <div className='news-photos'>
+                <div className='news-photo rounded-xl'>
+                  <img className='w-100' src={TanyaMikeManoj} alt="Tanya, Mike and Manoj at historic Man Sagar lake, Jaipur. Rupali Vardhan" />
+                  <div className='caption'>Tanya, Mike and Manoj at historic Man Sagar lake, Jaipur. Rupali Vardhan</div>
+                </div>
+                <div className='news-photo rounded-xl'>
+                  <img className='w-100' src={TanyaMikeHarsh} alt="(r to l) Tanya, Mike and Harsh at Jal Mahal, Jaipur. Rupali" />
+                  <div className='caption'>(r to l) Tanya, Mike and Harsh at Jal Mahal, Jaipur. Rupali</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
      </section>
     </div>
